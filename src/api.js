8c683b295a0651e44fbc6522f20ca57c1b9243cc
Rosie <rosie.bradshaw3@gmail.com>
2022-12-01 18:14:09 +0100

    ahora con algo de estilo

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 let button = document.createElement('button'); 
     document.body.appendChild(button);
 
+document.querySelector('button').innerHTML = 'Press me';
 
 button.addEventListener("click", (createNewFact)=>{
 
@@ -12,42 +13,6 @@ fetch('https://uselessfacts.jsph.pl/random.json?language=en')
         let newFactText = document.createTextNode(data.text);
         createNewFact.appendChild(newFactText);
         document.body.appendChild(createNewFact);
-
-        
-        console.log(newFactText)
-        
-       
-
-        return newFactText;
         })
-
-    
-        
-        
-        }
-        
-
-    )
- 
-
-
-//api ----> https://pokeapi.co/api/v2/pokemon/
-
-// const ulElement = document.querySelector('ul');
-
-// const createLiElement = (valor) =>{
-//     const liElement = document.createElement('li');
-//     liElement.innerHTML=valor;
-//     return liElement;
-// }
-
-// fetch('https://pokeapi.co/api/v2/pokemon/')
-//     .then(result => result.json())
-//     .then(data => {
-//         data.results.forEach(elemento => //data.results es el la posición donde empieza nuestro array (está dentro de un campo 'results')
-//             ulElement.appendChild(createLiElement(elemento.name))
-//         )
-//     })
-//     .catch(error => console.log(error))
-
-        
+    }
+)
